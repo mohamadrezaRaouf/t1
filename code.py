53369b547d66ccdfe187dd9cafa9a8f7ebe8a232
mohamadrezaRaouf <130048885+mohamadrezaRaouf@users.noreply.github.com>
2023-04-16 14:34:59 +0430

    adding two_d class which use inheritance

--- code.py
+++ code.py
@@ -30,3 +30,25 @@ class shape(ABC):
     def get_name(self,**kwargs):
         self._name=input('please enter your shape name: ')
         print(f'yor shapes name is {self._name}')
+class three_d(shape):
+    def __init__(self,side_area=None,total_area=None,**kwargs):
+        super().__init__()
+        self._side_area=side_area
+        self._total_area=total_area
+
+    def get_name(self):
+        super().get_name()
+        print('and this is a 3d shape')
+        
+    def side_area(self,**kwargs):
+        print(f'your side area is {self._side_area}')
+    
+    def total_area(self,**kwargs):
+        
+        print(f'your total area is {self._total_area}')
+    def Volume(self,**kwargs):
+        pass
+        
+    def Perimeter(self,**kwargs):
+        print('its 3d shape and dosent have perimeter')
+    
